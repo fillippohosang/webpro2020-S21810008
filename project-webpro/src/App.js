9c0451d3f776b7cb2721f9eb3a25ca51f6688e69
fillippohosang <fillippohosang@gmail.com>
2020-11-08 15:02:03 +0800

    Commited

---
+++ project-webpro/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Web Programming
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Welcome, S21810008
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
